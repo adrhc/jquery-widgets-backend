c2fad48b7eff548896751359a6ae6c8b0fe7cd51
adrhc <adrhc@outlook.com>
2020-11-06 17:30:36 +0200

    DataRestApplication: config.getCorsRegistry

--- src/main/java/ro/go/adrhc/datarest/DataRestApplication.java
+++ src/main/java/ro/go/adrhc/datarest/DataRestApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.Type;
+import java.util.EnumSet;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 @SpringBootApplication
 public class DataRestApplication {
@@ -19,6 +23,12 @@ public class DataRestApplication {
 		return RepositoryRestConfigurer.withConfig(config -> {
 			config.exposeIdsFor(entityManager.getMetamodel().getEntities()
 					.stream().map(Type::getJavaType).toArray(Class[]::new));
+			config.getCorsRegistry()
+					.addMapping("/**")
+					.allowedOrigins(ALL)
+					.allowedMethods(EnumSet.allOf(RequestMethod.class).stream().map(Enum::name).toArray(String[]::new))
+					.allowedHeaders(ALL)
+					.allowCredentials(true);
 		});
 	}
 }
