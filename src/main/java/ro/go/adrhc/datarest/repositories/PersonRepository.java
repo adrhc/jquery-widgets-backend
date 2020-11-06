d74cfd870f33375ef43c9e6d26de6382d6a86382
adrhc <adrhc@outlook.com>
2020-11-06 17:04:47 +0200

    PersonRepository: removed @CrossOrigin

--- src/main/java/ro/go/adrhc/datarest/repositories/PersonRepository.java
+++ src/main/java/ro/go/adrhc/datarest/repositories/PersonRepository.java
@@ -1,8 +1,6 @@
 package ro.go.adrhc.datarest.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import ro.go.adrhc.datarest.entities.Person;
 
-@CrossOrigin
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {}
