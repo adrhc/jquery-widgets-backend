0cb897322d3587ce89b657359227c53aef4030a9
Adrian Petre <adrian.petre@endava.com>
2020-11-07 18:32:34 +0200

    DataRestApplication: using CorsFilter application.properties: spring.jpa.open-in-view=false

--- src/main/java/ro/go/adrhc/datarest/DataRestApplication.java
+++ src/main/java/ro/go/adrhc/datarest/DataRestApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.Type;
-import java.util.EnumSet;
 
 import static org.springframework.web.cors.CorsConfiguration.ALL;
 
@@ -23,12 +24,20 @@ public class DataRestApplication {
 		return RepositoryRestConfigurer.withConfig(config -> {
 			config.exposeIdsFor(entityManager.getMetamodel().getEntities()
 					.stream().map(Type::getJavaType).toArray(Class[]::new));
-			config.getCorsRegistry()
-					.addMapping("/**")
-					.allowedOrigins(ALL)
-					.allowedMethods(EnumSet.allOf(RequestMethod.class).stream().map(Enum::name).toArray(String[]::new))
-					.allowedHeaders(ALL)
-					.allowCredentials(true);
 		});
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		//		config.setAllowedMethods(EnumSet.allOf(RequestMethod.class)
+		//				.stream().map(Enum::name).collect(Collectors.toList()));
+		config.addAllowedMethod(ALL);
+		config.addAllowedOrigin(ALL);
+		config.addAllowedHeader(ALL);
+		config.setAllowCredentials(true);
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
