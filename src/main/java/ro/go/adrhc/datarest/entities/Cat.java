2da5318c78676a1eefc81db142df5e8996e4cd90
adrhc <adrhc@outlook.com>
2020-12-10 21:49:01 +0200

    implements Serializable

--- src/main/java/ro/go/adrhc/datarest/entities/Cat.java
+++ src/main/java/ro/go/adrhc/datarest/entities/Cat.java
@@ -6,11 +6,12 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
-public class Cat {
+public class Cat implements Serializable {
 	@Id
 	@GeneratedValue
 	private Integer id;
--- src/main/java/ro/go/adrhc/datarest/entities/Person.java
+++ src/main/java/ro/go/adrhc/datarest/entities/Person.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class Person {
+public class Person implements Serializable {
 	@Id
 	@GeneratedValue
 	private Integer id;
